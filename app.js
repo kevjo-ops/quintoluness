1b362a546a243e42d86913f060cc30680e380cfe
kevjo-ops <kevinjonathanpilcohuillca@gmail.com>
2025-04-08 11:32:02 -0500

    a

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const showMessage=(message)=>{
-    return message;
+function busquedalineal(arr,elemento){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===elemento){
+            return i;
+        }
+    }
+    return -1;
 }
-console.log(showMessage('Hola como estas'))
-console.log("segundo mensage")
+
+const arreglo=[10,2,3,8,4,9,7];
+const elementobuscado=7;
+const indice= busquedalineal(arreglo,elementobuscado);
+console.log(`El elemento ${elementobuscado} se encuentra en el indice ${indice}`)
